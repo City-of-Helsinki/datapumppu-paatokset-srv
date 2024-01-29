d8e6f5decf00f03c4e30467bf02535854ce6f59b
Ville Palo <ville.palo@avanio.com>
2024-01-29 17:04:50 +0200

    Added some unit tests

--- AhjoApiServiceUnitTests/AhjoApi/AhjoApiClientTests.cs
+++ AhjoApiServiceUnitTests/AhjoApi/AhjoApiClientTests.cs
@@ -1,4 +1,5 @@
 using AhjoApiService.AhjoApi;
+using AhjoApiService.AhjoApi.DTOs;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Moq.Protected;
@@ -7,6 +8,84 @@ using Xunit;
 
 public class AhjoApiClientTests
 {
+    [Fact]
+    public async Task GetMeetingDetils_ReturnsDetails_WhenReponseIsSuccess()
+    {
+        var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Loose);
+        handlerMock
+           .Protected()
+           .Setup<Task<HttpResponseMessage>>(
+              "SendAsync",
+              ItExpr.IsAny<HttpRequestMessage>(),
+              ItExpr.IsAny<CancellationToken>()
+           )
+           .ReturnsAsync(new HttpResponseMessage()
+           {
+               StatusCode = HttpStatusCode.OK,
+               Content = new StringContent("{\"Meetings\":[{\"MeetingID\": \"1\"}]}"),
+           })
+           .Verifiable();
+
+        var loggerMock = new Mock<ILogger<AhjoApiClient>>();
+        var connectionMock = new Mock<IAhjoApiConnection>();
+        connectionMock.Setup(x => x.CreateConnection()).Returns(() => 
+        {
+            return new HttpClient(handlerMock.Object)
+            {
+                BaseAddress = new Uri("http://test.com/"), // use your real URL here
+            };
+        });
+        var client = new AhjoApiClient(loggerMock.Object, connectionMock.Object);
+        var result = await client.GetMeetingDetails(new AhjoMeetingDTO() { MeetingID = "2021-05-12T00:00:00" });
+
+        Assert.NotNull(result);
+        Assert.Equal("1", result?.MeetingID);
+        handlerMock.Protected().Verify(
+           "SendAsync",
+           Times.Exactly(1), // we expected a single external request
+           ItExpr.IsAny<HttpRequestMessage>(),
+           ItExpr.IsAny<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task GetDecisions_ReturnsDetails_WhenReponseIsSuccess()
+    {
+        var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Loose);
+        handlerMock
+           .Protected()
+           .Setup<Task<HttpResponseMessage>>(
+              "SendAsync",
+              ItExpr.IsAny<HttpRequestMessage>(),
+              ItExpr.IsAny<CancellationToken>()
+           )
+           .ReturnsAsync(new HttpResponseMessage()
+           {
+               StatusCode = HttpStatusCode.OK,
+               Content = new StringContent("{\"Decisions\":[]}"),
+           })
+           .Verifiable();
+
+        var loggerMock = new Mock<ILogger<AhjoApiClient>>();
+        var connectionMock = new Mock<IAhjoApiConnection>();
+        connectionMock.Setup(x => x.CreateConnection()).Returns(() => 
+        {
+            return new HttpClient(handlerMock.Object)
+            {
+                BaseAddress = new Uri("http://test.com/"), // use your real URL here
+            };
+        });
+        var client = new AhjoApiClient(loggerMock.Object, connectionMock.Object);
+        var result = await client.GetDecisions("1");
+
+        Assert.NotNull(result);
+        Assert.Equal(0, result?.Length);
+        handlerMock.Protected().Verify(
+           "SendAsync",
+           Times.Exactly(1), // we expected a single external request
+           ItExpr.IsAny<HttpRequestMessage>(),
+           ItExpr.IsAny<CancellationToken>());
+    }
+
     [Fact]
     public async Task GetMeetings_ReturnsMeetings_WhenResponseIsSuccess()
     {
